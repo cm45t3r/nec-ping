1673a0d3e56cc0a8b740fd369d5d99d3360cab25
unknown <ENE5594A@OBOG1331DKTP.comcel.com.co>
2013-07-26 10:40:52 -0500

    Added Dixon python ping and clean-up.

---
+++ python/lveping.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Copyright (C) 2013 NEC de Colombia/TX-NOC
+# Author: Carlos Uribe <cauribe@nec.com.co>
+
+import sys
+from ping import *
+
+if __name__ == '__main__':
+	address = sys.argv[1]
+	timeout = int(sys.argv[2])
+	size = int(sys.argv[3])
+	rtt = do_one(address, timeout, size)
+
+	if rtt != None:
+		rtt = rtt * 1000
+		print "%0.2f ms" % rtt
+	else:
+		print rtt
